13ae301882ac0bbfe127b423e802ff01870f2897
Asher-Thomas-Babu <asher.thomas.babu@ibm.com>
2024-12-04 10:26:56 +0530

    chore: fix imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import AppRouter from "./AppRouter";
 import NavBar from "./components/NavBar/NavBar";
 import Popup from "./components/Popup/Popup";
-import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
