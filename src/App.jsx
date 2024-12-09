faae019312424fa18c1badeac0a892ff2385b9be
AsherThomasBabu <ashertb2000@gmail.com>
2024-12-09 11:01:19 +0530

    feat: add BMC link

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
       </div>
       <SpeedInsights />
       <Analytics />
+      <a className="fixed bottom-5 left-1/2 transform -translate-x-1/2 z-50" href="https://www.buymeacoffee.com/asherthomas"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=asherthomas&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff" /></a>
     </>
   );
 }
